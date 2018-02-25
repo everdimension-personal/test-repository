d3228b8da5601001aac9b51c7e73afe307b11e7a
everdimension <everdimension@gmail.com>
2018-02-25 19:26:42 +0300

    add print to main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import webapp2
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
+        print 'hello'
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('hello, udacity')
 
