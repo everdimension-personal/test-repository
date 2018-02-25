c225d7fc6cdd56b7b7df9539506ccd665e3bd5d3
everdimension <everdimension@gmail.com>
2018-02-25 19:54:42 +0300

    add anotehr hello

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ print 'Start...'
 class MainPage(webapp2.RequestHandler):
     def get(self):
         print 'hello'
+        print 'another hello'
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('hello, udacity')
 
